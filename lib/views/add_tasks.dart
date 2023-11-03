5991285dae55d9320bed564488c57ea05e02261c
theLostPixel <nikhilchouhan2002@gmail.com>
2023-11-03 18:58:09 +0530

    added  'add tasks' method
    Only UI component added.  Backend code needed

--- lib/views/add_tasks.dart
+++ lib/views/add_tasks.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:taskified/consts/textstyle.dart';
 
 class AddTask extends StatefulWidget {
@@ -9,17 +10,63 @@ class AddTask extends StatefulWidget {
 }
 
 class _AddTaskState extends State<AddTask> {
+  TextEditingController titleCont = TextEditingController();
+  TextEditingController descCont = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
           backgroundColor: Colors.teal[200],
-          title: Text("Add Task", style: ourStyle(
-              size: 24,
-              color: Colors.black
-          ),
+          title: Text(
+            "Add Task",
+            style: ourStyle(size: 24, color: Colors.black),
             textAlign: TextAlign.center,
           )),
-      );
+      body: Container(
+          padding: const EdgeInsets.all(6.0),
+          child: Column(
+            children: [
+              Container(
+                child: const TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Enter the Title"),
+                ),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Container(
+                child: const TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Enter the content",
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Container(
+                  width: double.infinity,
+                  height: 56,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.resolveWith((Set<MaterialState> states){
+                        if(states.contains(MaterialState.pressed)) {
+                          return Colors.teal.shade50;
+                        }
+                        return Colors.teal.shade200;
+                      },)
+                    ),
+                    onPressed: (){},
+                    child: Text(
+                      "Add Task",
+                      style: GoogleFonts.roboto(fontSize: 15),
+                    ),
+                  ) ),
+            ],
+          )),
+    );
   }
 }
