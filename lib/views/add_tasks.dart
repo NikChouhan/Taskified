1480710ac985cf605430d34021e112b50ea3038f
theLostPixel <nikhilchouhan2002@gmail.com>
2023-11-05 12:15:58 +0530

    added ability to push data to the firebase DB

--- lib/views/add_tasks.dart
+++ lib/views/add_tasks.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:taskified/consts/textstyle.dart';
@@ -12,6 +13,23 @@ class AddTask extends StatefulWidget {
 class _AddTaskState extends State<AddTask> {
   TextEditingController titleCont = TextEditingController();
   TextEditingController descCont = TextEditingController();
+
+  addTaskToFirebase() {}
+
+  Future _addTasks({required String taskName, required String taskDesc}) async {
+    DocumentReference docRef =
+        await FirebaseFirestore.instance.collection('tasks').add(
+      {
+        'taskName': taskName,
+        'taskDesc': taskDesc,
+      },
+    );
+    String taskId = docRef.id;
+    await FirebaseFirestore.instance.collection('tasks').doc(taskId).update(
+      {'id': taskId},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,17 +45,21 @@ class _AddTaskState extends State<AddTask> {
           child: Column(
             children: [
               Container(
-                child: const TextField(
+                child: TextField(
+                  controller: titleCont,
+                  // ignore: prefer_const_constructors
                   decoration: InputDecoration(
                       border: OutlineInputBorder(),
                       labelText: "Enter the Title"),
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 12,
               ),
               Container(
-                child: const TextField(
+                child: TextField(
+                  controller: descCont,
+                  // ignore: prefer_const_constructors
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: "Enter the content",
@@ -60,7 +82,13 @@ class _AddTaskState extends State<AddTask> {
                         return Colors.teal.shade200;
                       },
                     )),
-                    onPressed: () {},
+                    onPressed: () {
+                      final taskName = titleCont.text;
+                      final taskDesc = descCont.text;
+                      _addTasks(taskName: taskName, taskDesc: taskDesc);
+
+                      Navigator.of(context, rootNavigator: true).pop();
+                    },
                     child: Text(
                       "Add Task",
                       style: GoogleFonts.roboto(fontSize: 15),
