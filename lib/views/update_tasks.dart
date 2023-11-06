287dbdbee621adafdaa45f07e7a9c80628156f70
theLostPixel <nikhilchouhan2002@gmail.com>
2023-11-06 13:40:56 +0530

    added olaceholder update_tasks.dart file

---
+++ lib/views/update_tasks.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UpdateTasks extends StatefulWidget {
+  const UpdateTasks({super.key});
+
+  @override
+  State<UpdateTasks> createState() => _UpdateTasksState();
+}
+
+class _UpdateTasksState extends State<UpdateTasks> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
